17daa740a6292f002d58b8622246aeb96528cd22
adengitstuff <adengitstuff@gmail.com>
2026-02-28 19:30:54 -0500

    parallel multi! i think i got the last piece to fully train. the rest, i'm familiar with through CNN's!

--- causalattention.py
+++ causalattention.py
@@ -29,24 +29,26 @@ class CausalAttention(nn.Module):
         context_vectors = attention_weights @ values
         return context_vectors
     
+if __name__ == "__main__":
 
-# Test:
-inputs = torch.tensor(
- [[0.43, 0.15, 0.89], # Your (x^1)
- [0.55, 0.87, 0.66], # journey (x^2)
- [0.57, 0.85, 0.64], # starts (x^3)
- [0.22, 0.58, 0.33], # with (x^4)
- [0.77, 0.25, 0.10], # one (x^5)
- [0.05, 0.80, 0.55]] # step (x^6)
-)
-batch = torch.stack((inputs, inputs), dim=0)
-print (batch.shape)
+    # Test:
+    inputs = torch.tensor(
+    [[0.43, 0.15, 0.89], # Your (x^1)
+    [0.55, 0.87, 0.66], # journey (x^2)
+    [0.57, 0.85, 0.64], # starts (x^3)
+    [0.22, 0.58, 0.33], # with (x^4)
+    [0.77, 0.25, 0.10], # one (x^5)
+    [0.05, 0.80, 0.55]] # step (x^6)
+    )
+    batch = torch.stack((inputs, inputs), dim=0)
+    print (batch.shape)
 
 
-torch.manual_seed(123)
-context_length = batch.shape[1]
-d_in = 3
-d_out = 2
-ca = CausalAttention(d_in, d_out, context_length, 0.0)
-context_vecs = ca(batch)
-print("context vecs.shape:", context_vecs.shape)
+    torch.manual_seed(123)
+    context_length = batch.shape[1]
+    d_in = 3
+    d_out = 2
+    ca = CausalAttention(d_in, d_out, context_length, 0.0)
+    context_vecs = ca(batch)
+    print("context vecs.shape:", context_vecs.shape)
+    print(context_vecs[0])
